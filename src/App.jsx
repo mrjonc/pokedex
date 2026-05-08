ae6d8a0cd2cad5cc2e3802157dd6bd640470526e
mrjonc <jonathanlaureano33.jl@gmail.com>
2026-05-08 13:07:19 -0300

    Ajustando o css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Outlet } from "react-router-dom";
 import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <>
       <NavBar search={search} setSearch={setSearch} />
